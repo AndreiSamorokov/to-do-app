57cacbea801e0f9fdf51579987e98582b49df8f4
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-18 02:26:29 +0900

    make complete

--- src/Todo.js
+++ src/Todo.js
@@ -25,6 +25,12 @@ function TodosComponent() {
         })
         setTodos(todosArray);
       }
+
+      function completeTodo( index ){
+          const todosArray = [...todos];
+          todosArray[index].isCompleted = !todosArray[index].isCompleted
+          setTodos(todosArray)
+      }
   return (
     <div>
         <input 
@@ -39,12 +45,16 @@ function TodosComponent() {
                     setCurrentTodo('');
                 }
             }}
-
             placeholder="Input anything"
-
         />
       {todos.map((todo, index) => (
-          <p> { todo.todo } </p>
+          <div className="todo" key={ todo }>
+                <div className="checkbox" onClick={() => completeTodo(index)}>
+                    {todo.isCompleted && <span>&#x2714;</span>}
+                </div>
+                <div className={todo.isCompleted ? "done" : ""}> { todo.todo } </div>
+          </div>
+          
       ))}
 
       <b>{todos.length > 0 && `${todos.length} items`}</b>
