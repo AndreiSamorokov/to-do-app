04c74f2251d45123f1fd76f9d0efd44b28fbcac4
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-18 02:08:41 +0900

    component add

---
+++ src/Todo.js
@@ -0,0 +1,31 @@
+import React, { useState }  from "react";
+
+function TodosComponent() {
+    const [currentTodo, setCurrentTodo] = useState("");
+    const [todos, setTodos] = useState([
+        {
+          todo: "bake a cake",
+          isCompleted: true
+        },
+        {
+          todo: "go for a walk",
+          isCompleted: false
+        },
+        {
+          todo: "contribute a web development tutorial on Enlight",
+          isCompleted: false
+        }
+      ]);
+
+  return (
+    <div>
+      {todos.map((todo, index) => (
+          <p> { todo.todo } </p>
+      ))}
+
+      <b>{todos.length > 0 && `${todos.length} items`}</b>
+    </div>
+  );
+}
+
+export default TodosComponent;
